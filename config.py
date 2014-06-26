b9f5176764cbd42ce503e6ef55c8badcab2f7167
Basel J. Hamadeh <hamadeh.basel@gmail.com>
2014-06-26 22:33:58 +0800

    Adds config file configs

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    APP_MAIL_SUBJECT_PREFIX = '[APP]'
+    APP_MAIL_SENDER = 'App Admin <app@example.com>'
+    APP_ADMIN = os.environ.get('APP_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smpt.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+config = { 'development': DevelopmentConfig, 'testing': TestingConfig, 'production': ProductionConfig, 'default': DevelopmentConfig}
+
+
+
